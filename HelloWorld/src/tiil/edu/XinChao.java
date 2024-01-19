234f6368c575f42f83975faa97e8205e30f8cf3d
TranNhutTrung1003 <125780142+TranNhutTrung1003@users.noreply.github.com>
2024-01-19 15:51:37 +0700

    ví dụ đầu tiên

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xin chao. toi la sinh vien thuoc khoa 63-CNTT3 cua truong dai hoc nha trang");
+	}
+
+}
