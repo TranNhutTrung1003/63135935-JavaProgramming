b69396c9c29ca8176aa130b5f9cfc1c671e5e413
TranNhutTrung1003 <125780142+TranNhutTrung1003@users.noreply.github.com>
2024-02-28 11:52:08 +0700

    Hoàn thiện TH_Bai1

--- TH_Bai1/src/tnt/tiil/edu/HomeFrame.java
+++ TH_Bai1/src/tnt/tiil/edu/HomeFrame.java
@@ -3,6 +3,9 @@ package tnt.tiil.edu;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class HomeFrame extends JFrame {
 
@@ -20,6 +23,13 @@ public class HomeFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Hệ thống quản lý bán hàng");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(102, 72, 394, 79);
+		contentPane.add(lblNewLabel);
 	}
 
 }
